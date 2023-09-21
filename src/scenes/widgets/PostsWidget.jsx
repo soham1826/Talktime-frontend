fd2c60c38f318cd032862ea16a0b8971749426fd
soham Kulkarni <soham18262@gmail.com>
2023-09-21 11:24:16 +0530

    some changes

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -14,7 +14,8 @@ const PostsWidget = ({ userId, isProfile = false }) => {
       headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
-    dispatch(setPosts({ posts: data }));
+    const newData = await data.reverse()
+    dispatch(setPosts({ posts: newData }));
   };
 
   const getUserPosts = async () => {
@@ -26,7 +27,8 @@ const PostsWidget = ({ userId, isProfile = false }) => {
       }
     );
     const data = await response.json();
-    dispatch(setPosts({ posts: data.reverse() }));
+    const newData = await data.reverse()
+    dispatch(setPosts({ posts:newData  }));
   };
 
   useEffect(() => {
